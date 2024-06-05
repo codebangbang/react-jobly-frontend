3d2aff7f1ff1943e4405169e6538d1260e5451ad
codebangbang <ryfair@gmail.com>
2024-06-05 19:01:00 -0400

    I think it is running properly now, but is not reading my database correctly.

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,18 @@
-// const { override, addWebpackResolve } = require("customize-cra");
-// const path = require("path");
+const webpack = require('webpack');
 
-// module.exports = override(
-//   addWebpackResolve({
-//     fallback: {
-//       crypto: require.resolve("crypto-browserify"),
-//     },
-//   })
-// );
+module.exports = function override(config, env) {
+  // Add the plugin
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  );
+
+  // Add a fallback for the 'crypto' module
+  config.resolve.fallback = { 
+    ...config.resolve.fallback, 
+    "crypto": require.resolve("crypto-browserify") 
+  };
+
+  return config;
+};
