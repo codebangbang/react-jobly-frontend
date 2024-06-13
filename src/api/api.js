682f684b192a3b3b39a4f0e6f6410d75166740bb
codebangbang <ryfair@gmail.com>
2024-06-13 17:50:31 -0400

    updated api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001" || "https://react-jobly-backend-6kw1.onrender.com/";
+const BASE_URL = "https://react-jobly-backend-6kw1.onrender.com/";
+//  process.env.REACT_APP_BASE_URL || "http://localhost:3001" ||;
 
 /** API Class.
  *
